16fd69eec0d57c99d10de8dbd7be21f026e87866
Collins Otieno <otienocollins0549@gmail.com>
2025-08-08 23:07:08 +0300

    feat: Initialize React app with Vite and Tailwind CSS and laravel backend

---
+++ frontend/react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello, Digital Leap Africa!
+      </h1>
+    </div>
+  )
+}
+
+export default App
