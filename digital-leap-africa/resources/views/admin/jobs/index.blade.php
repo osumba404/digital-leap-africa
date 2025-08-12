3836482453fcc6b2e5f829429f4e44c89561ce45
Collins Otieno <otienocollins0549@gmail.com>
2025-08-12 22:20:10 +0300

    feat: Finalize UI refinements for responsiveness and dynamic branding

--- digital-leap-africa/resources/views/admin/jobs/index.blade.php
+++ digital-leap-africa/resources/views/admin/jobs/index.blade.php
@@ -14,31 +14,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-primary-light shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-100">
-                    <table class="w-full text-left">
-                        <thead>
-                            <tr class="border-b border-gray-700">
-                                <th class="p-2">Title</th>
-                                <th class="p-2">Company</th>
-                                <th class="p-2">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($jobs as $job)
+                    <div class="overflow-x-auto">
+                        <table class="w-full text-left">
+                            <thead>
                                 <tr class="border-b border-gray-700">
-                                    <td class="p-2">{{ $job->title }}</td>
-                                    <td class="p-2">{{ $job->company }}</td>
-                                    <td class="p-2">
-                                        <a href="{{ route('admin.jobs.edit', $job) }}" class="text-accent hover:text-white">Edit</a>
-                                        <form method="POST" action="{{ route('admin.jobs.destroy', $job) }}" class="inline-block ml-4" onsubmit="return confirm('Are you sure you want to delete this job?');">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="text-red-500 hover:text-red-400">Delete</button>
-                                        </form>
-                                    </td>
+                                    <th class="p-2">Title</th>
+                                    <th class="p-2">Company</th>
+                                    <th class="p-2">Actions</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($jobs as $job)
+                                    <tr class="border-b border-gray-700">
+                                        <td class="p-2">{{ $job->title }}</td>
+                                        <td class="p-2">{{ $job->company }}</td>
+                                        <td class="p-2">
+                                            <a href="{{ route('admin.jobs.edit', $job) }}" class="text-accent hover:text-white">Edit</a>
+                                            <form method="POST" action="{{ route('admin.jobs.destroy', $job) }}" class="inline-block ml-4" onsubmit="return confirm('Are you sure you want to delete this job?');">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-500 hover:text-red-400">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
