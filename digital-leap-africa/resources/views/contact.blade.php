06d35b411a5fae6b1a8109a51e9823b1e48af7f9
Evans Osumba <osumbaevanzz@gmail.com>
2025-10-07 16:49:31 +0300

    error free

---
+++ digital-leap-africa/resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title','Contact')
+@section('content')
+<div class="container">
+  <div class="card">
+    <h1 style="margin-top:0;">Contact Us</h1>
+    <p style="color:var(--cool-gray)">Reach out and we’ll get back to you.</p>
+  </div>
+</div>
+@endsection
---
+++ digital-leap-africa/resources/views/donate.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title','Donate')
+@section('content')
+<div class="container">
+  <div class="card">
+    <h1 style="margin-top:0;">Donate</h1>
+    <p style="color:var(--cool-gray)">Support our initiatives.</p>
+  </div>
+</div>
+@endsection
